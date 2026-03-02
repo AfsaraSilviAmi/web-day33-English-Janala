d88f894b4dbca1f2e19f5bb70f133735e8b36263
AfsaraSilviAmi <afsarasilvi44@gmail.com>
2026-03-03 02:24:15 +0600

    Did the error handling part

--- script/words.js
+++ script/words.js
@@ -11,6 +11,8 @@ const loadWordCard = (id) =>{
     .then((respond)=>respond.json())
     .then((data)=>displayWordCard(data.data))
 }
+
+//btn
 const displayLessonBtn = (lessons) =>{
      let lessonDivContainer = document.getElementById("lesson-btn-container");
      lessonDivContainer.innerHTML = "";
@@ -18,7 +20,7 @@ const displayLessonBtn = (lessons) =>{
         let lessonBtnDiv = document.createElement("div");
         lessonBtnDiv.innerHTML = `
        
-        <button onclick ="loadWordCard(${lesson.level_no})"class="btn btn-outline btn-primary px-6 rounded-md text-lg py-6"><i class="fa-solid fa-book-open"></i>Lesson-${lesson.level_no}</button>
+        <button onclick ="loadWordCard(${lesson.level_no})"class="btn btn-outline btn-primary px-4 rounded-md text-md py-6"><i class="fa-solid fa-book-open"></i>Lesson-${lesson.level_no}</button>
         
         `
         lessonDivContainer.append(lessonBtnDiv); 
@@ -26,17 +28,26 @@ const displayLessonBtn = (lessons) =>{
     
 }
 
+//card
 const displayWordCard = (words) =>{
     const cardDiv = document.getElementById("word-container");
     cardDiv.innerHTML="";
-    
+    if(words.length == 0){
+        cardDiv.innerHTML=`
+          <div class="col-span-full space-y-5 p-8">
+               <img src="./assets/alert-error.png" alt="" class="mx-auto">
+                <p class="font-bangla text-[#79716B] text-lg">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+                <h1 class="font-bangla text-4xl">নেক্সট Lesson এ যান</h1>
+            </div>
+        `;
+    }
     words.forEach(word => {
         let card = document.createElement("div");
         card.innerHTML = `
         <div class="bg-white p-10 space-y-4 rounded-lg">
-                <h2 class="font-bold text-2xl">${word.word}</h2>
+                <h2 class="font-bold text-2xl">${word.word? word.word: "No words found"}</h2>
                 <p class="font-medium">Meaning /Pronunciation</p>
-                <h2 class="font-semibold text-2xl text-gray-800">${word.meaning} / ${word.pronunciation}</h2>
+                <h2 class="font-semibold text-2xl text-gray-800 font-bangla">"${word.meaning? word.meaning:"Meaning not found"} / ${word.pronunciation? word.pronunciation: "Pronunciation not found"}"</h2>
                 <div class="flex justify-between">
                     <button class="btn bg-[#1A91FF20]"><i class="fa-solid fa-circle-info"></i></button>
                     <button class="btn bg-[#1A91FF20]"> <i class="fa-solid fa-volume-high"></i></button>
